c0ddadef0a39a430e58cc1af80de1781ea4d19c9
AfifBouzidi <afif.bouzidi@gmail.com>
2019-01-19 23:03:51 +0100

    in Memory Authentication

---
+++ prj-03/src/main/java/tn/abouzidi/Prj03Application.java
@@ -0,0 +1,14 @@
+package tn.abouzidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj03Application.class, args);
+	}
+
+}
+
