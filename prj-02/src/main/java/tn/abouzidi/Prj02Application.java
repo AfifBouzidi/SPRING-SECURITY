547902ad96fb2c530ae32af3fd3d9a422ea292b4
AfifBouzidi <afif.bouzidi@gmail.com>
2019-01-19 22:59:42 +0100

    default spring security

---
+++ prj-02/src/main/java/tn/abouzidi/Prj02Application.java
@@ -0,0 +1,14 @@
+package tn.abouzidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj02Application.class, args);
+	}
+
+}
+
